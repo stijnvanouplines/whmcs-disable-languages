8148ecc9f2d2f6a1247a7ffb2933fd8017cd4faf
Stijn Vanouplines <stijn@solitweb.be>
2018-12-09 16:32:02 +0100

    change  to  and return

--- modules/addons/disable_languages/hooks.php
+++ modules/addons/disable_languages/hooks.php
@@ -2,18 +2,16 @@
 
 use WHMCS\Database\Capsule;
 
-add_hook( 'ClientAreaPage', 1, function( array $params ) {
-    $locales = $params['locales'];
-
+add_hook( 'ClientAreaPage', 1, function( array $vars ) {
     $getEnabled = Capsule::table( 'mod_disable_languages' )->first();
-
     $enabled = json_decode( $getEnabled->enabled );
-     
+
+    $locales = $vars['locales'];
     foreach ( $locales as $key => $locale ) { 
         if ( ! in_array( $locale['language'], $enabled ) ) {
             unset( $locales[$key] ); 
         }
     }
-     
-    return array( 'locales' => $locales );
+
+    return $vars;
 });
